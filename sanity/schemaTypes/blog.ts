f64dac4fd91448d691eed1f5a2b3e813f72524ca
Daniel seta <danielseta37@gmail.com>
2025-02-07 13:04:19 -0800

    Navbar component and update homepage layout; enhance blog schema with slug options

--- sanity/schemaTypes/blog.ts
+++ sanity/schemaTypes/blog.ts
@@ -14,6 +14,9 @@ export default {
       name: 'slug',
       type: 'slug',
       title: 'Slug of your blog article',
+      options: {
+        source: 'title',
+      },
     },
     {
       name: 'titleImage',
