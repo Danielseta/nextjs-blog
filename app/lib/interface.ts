108d9d3d09242f756d9e6ff06c5c1033b447e4b4
Daniel seta <danielseta37@gmail.com>
2025-02-10 14:32:57 -0800

    mon-production

---
+++ app/lib/interface.ts
@@ -0,0 +1,6 @@
+export interface simpleBlogCard {
+  title: string;
+  smallDescription: string;
+  currentString: string;
+  titleImage: any;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
+import { client, urlFor } from "./lib/sanity";
+import { simpleBlogCard } from "./lib/interface";
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
-import Navbar from "./componets/Navbar";
 
-export default function Home() {
+async function getData() {
+  const query = `
+  *[_type == 'blog'] | order(_createdAt desc) {
+  title,
+    smallDescription,
+    "currentSlug": slug.current,
+     titleImage
+
+} `;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData();
+
   return (
-    <div>
-      <h1>hello there</h1>
+    <div className=" grid grid-cols-1 md:grid-cols-2 mt-5">
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt="image"
+            width={500}
+            height={500}
+          />
+        </Card>
+      ))}
     </div>
   );
 }
