dd1dacddc9048d2f37c394fa69e9b10c066e6afc
Daniel seta <danielseta37@gmail.com>
2025-02-10 15:09:59 -0800

    update page.tsx and card.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { client, urlFor } from "./lib/sanity";
 import { simpleBlogCard } from "./lib/interface";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 
 async function getData() {
@@ -22,7 +22,7 @@ export default async function Home() {
   const data: simpleBlogCard[] = await getData();
 
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-2 mt-5">
+    <div className=" grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
       {data.map((post, idx) => (
         <Card key={idx}>
           <Image
@@ -30,7 +30,14 @@ export default async function Home() {
             alt="image"
             width={500}
             height={500}
+            className=" rounded-t-lg h-[200px] object-cover"
           />
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2">{post.title}</h3>
+            <p className=" line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-400">
+              {post.smallDescription}
+            </p>
+          </CardContent>
         </Card>
       ))}
     </div>
