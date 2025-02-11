b0ebfcdccb05a595fc5378ac81fc0785f381d2f8
Daniel seta <danielseta37@gmail.com>
2025-02-11 03:32:35 -0800

    Refactor getStaticPaths to accept slug parameter and fetch blog data; update BlogArticle component to use async data retrieval

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,16 @@
-async function getStaticPaths() {
-
+async function getStaticPaths(slug: string) {
+    const query = `
+      *[_type == 'blog' && slug.current == '${slug}'] {
+  "currentSlug": slug.current,
+    title,
+    content,
+    titleImage
+}[0] `; 
+    const data = await client.fetch(query);
+    return data;
 }
 
-export default function BlogArticle({ params }: { params: { slug: string } }) {
+export default async function BlogArticle({ params }: { params: { slug: string } }) {
+    const data = await getData(params.slug);
     return <h1>{params.slug}</h1>
-}
+} 
