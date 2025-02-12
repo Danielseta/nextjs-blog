905c0e08fd1646232d405a18a18bb95ea7437545
Daniel seta <danielseta37@gmail.com>
2025-02-12 02:14:33 -0800

    BlogArticle component to use new data fetching method; enhance interface for full blog data

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,36 @@
-async function getStaticPaths(slug: string) {
-    const query = `
-      *[_type == 'blog' && slug.current == '${slug}'] {
-  "currentSlug": slug.current,
+import { fulLBlog } from "@/app/lib/interface";
+import { client } from "../../lib/sanity";
+
+async function getData(slug: string) {
+  const query = `
+    *[_type == "blog" && slug.current == '${slug}'] {
+    "currentSlug": slug.current,
     title,
     content,
     titleImage
-}[0] `; 
-    const data = await client.fetch(query);
-    return data;
+    }[0]`;
+
+  const data = await client.fetch(query);
+  return data;
 }
 
-export default async function BlogArticle({ params }: { params: { slug: string } }) {
-    const data = await getData(params.slug);
-    return <h1>{params.slug}</h1>
-} 
+export default async function BlogArticle({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: fulLBlog = await getData(params.slug);
+
+  return (
+    <div>
+      <h1>
+        <span className=" block text-base text-center text-primary font-semibold tracking-wide uppercase">
+          Daniel - Blog
+        </span>
+        <span className=" mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+          {data.title}
+        </span>
+      </h1>
+    </div>
+  );
+}
--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,7 +1,14 @@
 export interface simpleBlogCard {
-  currentSlug: any;
+  currentSlug: string;
   title: string;
   smallDescription: string;
   currentString: string;
+  titleImage: any;
+}
+
+export interface fulLBlog {
+  currentSlug: string;
+  title: string;
+  content: any;
   titleImage: any;
 }
